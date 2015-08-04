1d40a1bb22e99a1249e3d23ceb34657fde3a1c60
Ivan Magda <imagda15@gmail.com>
2015-08-04 21:46:28 +0300

    Add vibrant label on splash screen when touch id enabled.

--- Depoza/AppDelegate.m
+++ Depoza/AppDelegate.m
@@ -333,6 +333,19 @@ NSString * const StatusBarTappedNotification = @"statusBarTappedNotification";
         UIVisualEffectView *vibrancyEffectView = [[UIVisualEffectView alloc]initWithEffect:vibrancyEffect];
         [vibrancyEffectView setFrame:_tabBarController.view.bounds];
         
+        // Label for vibrant text
+        UILabel *vibrantLabel = [UILabel new];
+        [vibrantLabel setText:NSLocalizedString(@"ToThePenny", @"App name for vibrant label")];
+        [vibrantLabel setFont:[UIFont fontWithName:@"HelveticaNeue-CondensedBold" size:21.0]];
+        [vibrantLabel sizeToFit];
+        
+        CGPoint location = _tabBarController.view.center;
+        location.y = CGRectGetHeight([UIApplication sharedApplication].statusBarFrame) + 22.0f;
+        [vibrantLabel setCenter:location];
+        
+        // Add label to the vibrancy view
+        [[vibrancyEffectView contentView]addSubview:vibrantLabel];
+        
             // Add the vibrancy view to the blur view
         [[blurEffectView contentView]addSubview:vibrancyEffectView];
         
